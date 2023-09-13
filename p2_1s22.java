bf32215518e5d387824be14177089c0ab4d314a8
Angel Sebastian Baltazar Mijangos <144191254+AngelSeba@users.noreply.github.com>
2023-09-13 12:59:41 -0600

    Delete p2_1s22.java
    Se borr

--- p2_1s22.java
+++
@@ -1,9 +0,0 @@
-public class p2_1s22{
-	public static void main(String[] args) {
-		System.out.println("Angel Sebastian Baltazar Mijangos");
-		System.out.println("Tengo 18 años");
-		System.out.println("angelsebastianbaltazar@gmail.com");
-		System.out.println("Me gusta jugar videojuegos");
-		System.out.println("Mi color favorito es el negro");
-	}
-}
